7eafe21c3127db415d98856f0195376b385746c6
Chris George <chrisgeo@users.noreply.github.com>
2026-08-04 10:14:24 -0700

    fix(providersync): isolate live oracle imports

--- internal/providersync/testdata/oracle_pairs/github_work-items_sprint.py
+++ internal/providersync/testdata/oracle_pairs/github_work-items_sprint.py
@@ -12,6 +12,11 @@ from typing import Any
 
 from internal.providersync.testdata import oracle_registry
 from internal.providersync.testdata.field_reflection import dataclass_field_names
+from internal.providersync.testdata.oracle_pairs._github_work_items_helpers import (
+    install_minimal_oracle_imports,
+)
+
+install_minimal_oracle_imports()
 
 with (
     contextlib.redirect_stdout(io.StringIO()),
